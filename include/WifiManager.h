64bcd0b05de45383cca4afdddec5b3abd1bcb7c9
nickelbert <nickelbert@users.noreply.github.com>
2026-08-16 11:10:55 +0200

    feat: automatically reconnect wifi in ap mode (#2)

--- include/WifiManager.h
+++ include/WifiManager.h
@@ -4,6 +4,7 @@
 #include <cstdint>
 #include "esp_netif.h"
 #include "esp_wifi.h"
+#include "esp_timer.h"
 
 class WifiManager
 {
@@ -13,8 +14,12 @@ private:
     std::string ipAddress = "";
     esp_netif_t *staNetif = nullptr;
     esp_netif_t *apNetif = nullptr;
+    esp_timer_handle_t reconnectTimer = nullptr;
 
     static void eventHandler(void *arg, esp_event_base_t eventBase, int32_t eventId, void *eventData);
+    static void reconnectTimerCallback(void *arg);
+    void startReconnectTimer();
+    void stopReconnectTimer();
 
 public:
     void setup();
